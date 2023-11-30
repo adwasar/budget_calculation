697bf1d316690348f5623ca5ab5e61674e1b43a3
Vladislav <104913853+Adwasar@users.noreply.github.com>
2023-11-30 11:54:27 +0200

    feat: counter total balance in statistics page

--- src/Pages/PageStatistics.jsx
+++ src/Pages/PageStatistics.jsx
@@ -2,17 +2,36 @@ import { getSnapshot } from 'mobx-state-tree';
 
 import LoginMessage from '../components/LoginMessage';
 import { isLogin, recordsHistory } from '../storage';
+import { useEffect } from 'react';
 
 function PageStatistics() {
+  const data = getSnapshot(recordsHistory).records;
+  const totalBalance = data.reduce((acc, el) => acc + el.balance, 0);
+
   return (
     <>
       <div className="container">
-        <div className="s-60"></div>
-
         <div>
           {getSnapshot(isLogin).state ? (
             <>
-              <div>Record history: {JSON.stringify(getSnapshot(recordsHistory))}</div>
+              <div className="s-60"></div>
+              <div className={totalBalance >= 0 ? 'c-green' : 'c-red'}>Total: {totalBalance}</div>
+              <div className="s-20"></div>
+              <ul>
+                {data.map((el, i) => {
+                  return (
+                    <div key={i}>
+                      <li className="border p-2">
+                        <div>date: {el.date}</div>
+                        <div className={el.balance >= 0 ? 'c-green' : 'c-red'}>
+                          budget: {el.balance}
+                        </div>
+                      </li>
+                      <div className="s-10"></div>
+                    </div>
+                  );
+                })}
+              </ul>
             </>
           ) : (
             <LoginMessage />
